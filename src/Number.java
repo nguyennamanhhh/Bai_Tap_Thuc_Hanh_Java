32c0d683de67ef4145e8ed01258ec20c5ebe666b
Nam Anh <nguyennamanh2k4@gmail.com>
2026-07-15 09:41:01 +0700

    Update program to accept user input for 10 integers

--- src/Number.java
+++ src/Number.java
@@ -1,13 +1,29 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Number {
     public static void main(String[] args) {
-        int[] numbers = { 45, 12, 85, 32, 89, 39, 69, 10, 42, 1 };
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[10];
+
+        System.out.println("Nhap vao 10 so nguyen:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Phan tu thu " + (i + 1) + ": ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Vui long nhap mot so nguyen hop le!");
+                System.out.print("Phan tu thu " + (i + 1) + ": ");
+                scanner.next();
+            }
+            numbers[i] = scanner.nextInt();
+        }
 
         System.out.println("Mang ban dau: " + Arrays.toString(numbers));
 
         Arrays.sort(numbers);
 
         System.out.println("Mang sau khi sap xep tang dan: " + Arrays.toString(numbers));
+        
+        scanner.close();
     }
 }
+
